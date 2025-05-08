c96d4186ef83a95acdd94f3df5e9001d0335cb37
genti-ms <genti.ms10@icloud.com>
2025-05-08 12:59:33 +0200

    feat: add update functionality and bug fixes

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def update(post_id):
     return render_template('update.html', post=post)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
