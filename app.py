c5125a26d2aed88f679b582dfa0584a801c71a63
genti-ms <genti.ms10@icloud.com>
2025-05-08 12:10:52 +0200

    Index-Route hinzugefügt und JSON-Datenspeicherung implementiert

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def lade_posts():
+    with open('posts.json', 'r') as file:
+        return json.load(file)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    posts = lade_posts()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
